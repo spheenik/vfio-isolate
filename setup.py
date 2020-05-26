74efc1ec4902866b72dd8068b505cb0900b9dd85
Martin Schrodt <martin@schrodt.org>
2020-05-26 23:17:29 +0200

    up version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='vfio-isolate',
-    version='0.3.0',
+    version='0.3.1',
     description='Commandline tool to facilitate CPU core isolation',
     long_description=long_description,
     long_description_content_type='text/markdown',
